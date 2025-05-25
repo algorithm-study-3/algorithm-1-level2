2c38e3b382c9eb9e0a0c4d638ac4b7307eda614b
SuHwa Lee <tnghk9611@naver.com>
2025-05-25 11:39:21 +0900

    05.25 모음사전

---
+++ 2025-05/05.25/84512.js
@@ -0,0 +1,29 @@
+function solution(word) {
+  var answer = 0;
+
+  const arr = ["A", "E", "I", "O", "U"];
+
+  let cnt = 0;
+  let find = false;
+  const dfs = (depth, res) => {
+    if (res === word) {
+      find = true;
+      answer = cnt;
+      return;
+    }
+
+    cnt++;
+
+    if (find || depth === 5) {
+      return;
+    }
+
+    for (let i = 0; i < 5; i++) {
+      dfs(depth + 1, res + arr[i]);
+    }
+  };
+
+  dfs(0, "");
+
+  return answer;
+}
